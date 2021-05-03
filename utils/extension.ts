025efdb7dd2c767ddf412ed1bf9d743da5d8d09a
Gabe Rodriguez <gabe@spotify.com>
2021-05-03 15:56:52 +0200

    hacky fix to cleanup listener sdk issue

--- utils/extension.ts
+++ utils/extension.ts
@@ -24,24 +24,32 @@ interface TransactionRequest {
   address: string;
 }
 
+// Definitely hacky, but a limitation on SDK doesn't allow us to cleanup listeners
+let transactionCount = 0;
+let onConnectCount = 0;
+
 export const sendTransactionWithExtension = async (
   merchantAddress: string,
   price: number,
 ): Promise<TransactionResponse> =>
   new Promise((resolve, reject) => {
+    transactionCount++;
     const extension = new Extension();
     extension.connect();
 
     extension.on('onConnect', (w: { address: string }) => {
-      const toSend = new MsgSend(w.address, merchantAddress, {
-        uusd: price * 1000000, // micro-dollars
-      });
-      extension.post({
-        msgs: [toSend],
-        purgeQueue: true,
-        waitForConfirmation: true,
-        fee: new StdFee(1000000, [new Coin(Denom.USD, 1000000)]),
-      });
+      if (onConnectCount < transactionCount) {
+        const toSend = new MsgSend(w.address, merchantAddress, {
+          uusd: price * 1000000, // micro-dollars
+        });
+        extension.post({
+          msgs: [toSend],
+          purgeQueue: true,
+          waitForConfirmation: true,
+          fee: new StdFee(1000000, [new Coin(Denom.USD, 1000000)]),
+        });
+        onConnectCount++;
+      }
     });
 
     extension.on((payload: TransactionResponse | TransactionRequest) => {
